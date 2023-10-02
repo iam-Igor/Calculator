1cb25add076c1271d9d4802554939c4c0f5aa533
“Ygor <igorgarofalo@gmail.com>
2023-10-02 19:05:15 +0200

    added dry code

--- script/script.js
+++ script/script.js
@@ -6,80 +6,62 @@ const equal = document.getElementById("equal");
 
 const allNumbers = document.querySelectorAll(".number-chosen");
 
-const operators = document.getElementsByClassName("operators2");
+const allOperators = document.getElementsByClassName("operators2");
 const operatorsDisplay = document.getElementById("operatorsDisplay");
 
 let firstNumber = document.getElementById("num1");
 let secondNumber = document.getElementById("num2");
 
-let isfirstNum = false;
+let isfirstNum = true;
 
 const addEventToNumbers = function () {
   for (let i = 0; i < allNumbers.length; i++) {
     allNumbers[i].addEventListener("click", function () {
       const number = parseInt(allNumbers[i].innerText);
-
-      if (!isfirstNum) {
+      if (isfirstNum) {
         firstNumber.innerText += number;
-        isfirstNum = true;
-      }
-
-      for (let x = 0; x < operators.length; x++) {
-        operators[x].addEventListener("click", function () {
-          operatorsDisplay.innerText = operators[x].innerText;
-        });
-
-        if (
-          operatorsDisplay.innerText === "+" ||
-          operatorsDisplay.innerText === "-" ||
-          operatorsDisplay.innerText === "x" ||
-          operatorsDisplay.innerText === "/"
-        ) {
-          secondNumber.innerText = number;
-        }
-      }
-      if (operatorsDisplay.innerText === "+") {
-        equal.addEventListener("click", function () {
-          let result =
-            parseInt(firstNumber.innerText) + parseInt(secondNumber.innerText);
-          firstNumber.innerText = result;
-          operatorsDisplay.innerText = "";
-          secondNumber.innerText = "";
-        });
-      } else if (operatorsDisplay.innerText === "-") {
-        equal.addEventListener("click", function () {
-          let result =
-            parseInt(firstNumber.innerText) - parseInt(secondNumber.innerText);
-          firstNumber.innerText = result;
-          operatorsDisplay.innerText = "";
-          secondNumber.innerText = "";
-        });
-      } else if (operatorsDisplay.innerText === "x") {
-        equal.addEventListener("click", function () {
-          let result =
-            parseInt(firstNumber.innerText) * parseInt(secondNumber.innerText);
-          firstNumber.innerText = result;
-          operatorsDisplay.innerText = "";
-          secondNumber.innerText = "";
-        });
-      } else if (operatorsDisplay.innerText === "/") {
-        equal.addEventListener("click", function () {
-          let result =
-            parseInt(firstNumber.innerText) / parseInt(secondNumber.innerText);
-          firstNumber.innerText = result;
-          operatorsDisplay.innerText = "";
-          secondNumber.innerText = "";
-        });
+      } else {
+        secondNumber.innerText += number;
       }
     });
   }
+
+  const calculateResult = function () {
+    const num1 = parseInt(firstNumber.innerText);
+    const num2 = parseInt(secondNumber.innerText);
+
+    const operat = operatorsDisplay.innerText;
+
+    switch (operat) {
+      case "+":
+        return num1 + num2;
+      case "-":
+        return num1 - num2;
+      case "x":
+        return num1 * num2;
+      case "/":
+        return num1 / num2;
+      default:
+        return NaN;
+    }
+  };
+
+  for (let x = 0; x < allOperators.length; x++) {
+    allOperators[x].addEventListener("click", function () {
+      operatorsDisplay.innerText = allOperators[x].innerText;
+      isfirstNum = false;
+    });
+  }
+  equal.addEventListener("click", function () {
+    firstNumber.innerText = calculateResult();
+    secondNumber.innerText = "";
+    operatorsDisplay.innerText = "";
+    isfirstNum = true;
+  });
 };
+
 addEventToNumbers();
 
-dotSymbol.addEventListener("click", function () {
-  displayViewer.innerText += dotSymbol.innerText;
-});
-
 deleteSymbol.addEventListener("click", function () {
-  displayViewer.innerText = "";
+  location.reload();
 });
